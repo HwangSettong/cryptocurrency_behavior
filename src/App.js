2ed32c52c204eb99a86371b20f9c5c3a26e256ed
SettongHwang <devsehyun@gmail.com>
2022-07-01 11:52:19 +0900

    upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {useState, useEffect} from "react"
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([])
+  const [amount, setAmount] = useState(0)
+  const [price, setPrice] = useState(amount)
+  const onClick = () => (setPrice(amount))
+  const onChange = (event) => (setAmount(event.target.value))
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => setCoins(json));
+    
+    setLoading(false)
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {loading ? <strong>Loading...</strong> :
+      <div>
+        <div>
+          <h3>How much do you have? (USD)</h3>
+          <input value={amount} onChange={onChange} type="number" placeholder="USD"></input> 
+          <button onClick={onClick}>submit</button>
+        </div>
+        <div>
+          <h1>The Coins!{loading ? "" : ` (total : ${coins.length})`}</h1>
+          <table>
+            <th>Coin name</th>
+            <th>Coin price (USD)</th>
+            <th>I can have...</th>
+            {coins.map((coin) => 
+              <tr key={coin.id}>
+                <td>{coin.name} ({coin.symbol})</td>
+                <td>${coin.quotes.USD.price}</td>
+                <td>{price/coin.quotes.USD.price} coin</td>
+              </tr>)}
+          </table>
+        </div>
+      </div>
+      }
     </div>
   );
 }
-
+// coin.quotes.USD.price
 export default App;
